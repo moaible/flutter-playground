74f52a3009c0a347047f5198e63b4d3814a61c21
moaible <moai.motodera@gmail.com>
2021-12-03 19:37:14 +0900

    Add second page component

---
+++ lib/second_page/second_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class SecondPage extends HookWidget {
+  const SecondPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = useState(100);
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: SelectableText(title)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SelectableText(
+              'You have pushed the button this many times:',
+            ),
+            SelectableText(
+              '${counter.value}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.value--,
+        tooltip: 'Decrement',
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
---
+++ lib/second_page/second_page_stories.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+import 'package:playground/second_page/second_page.dart';
+
+Widget secondPage() => const SecondPage(title: "Decrement");
